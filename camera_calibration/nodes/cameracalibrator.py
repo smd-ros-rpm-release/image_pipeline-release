66f2ea4756c08621404981069353c7be7bd5f752
Vincent Rabaud <vincent.rabaud@gmail.com>
2014-09-21 12:04:34 +0200

    Rebase from 'upstream'

--- camera_calibration/nodes/cameracalibrator.py
+++ camera_calibration/nodes/cameracalibrator.py
@@ -40,9 +40,9 @@ from message_filters import ApproximateTimeSynchronizer
 
 import os
 try:
-    from queue import Queue
+    from queue import Queue, Empty as QueueEmptyException
 except ImportError:
-    from Queue import Queue
+    from Queue import Queue, Empty as QueueEmptyException
 import threading
 import functools
 
@@ -149,9 +149,17 @@ class CalibrationNode:
         pass
 
     def queue_monocular(self, msg):
+        try:
+            self.q_mono.get(False, 0)
+        except QueueEmptyException:
+            pass
         self.q_mono.put(msg)
 
     def queue_stereo(self, lmsg, rmsg):
+        try:
+            self.q_stereo.get(False, 0)
+        except QueueEmptyException:
+            pass
         self.q_stereo.put((lmsg, rmsg))
 
     def handle_monocular(self, msg):
